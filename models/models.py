4949362922263c87bf8ecd79c50da7a5d3fcb461
ProudHoosier <shashank@umail.iu.edu>
2017-04-03 18:17:42 -0400

    sick ML and GST

--- models/models.py
+++ models/models.py
@@ -1,10 +1,17 @@
 #!/Users/fa/anaconda/bin/python
 import sys
+import os
+sys.path.append('/home/ds/STS/FeatureBased')
+import imp
+imp.find_module('FBSimilarityMeasures', ['/home/ds/STS/FeatureBased'])
+imp.find_module('utils', ['/home/ds/STS/utils'])
+
 sys.path = ['../utils'] + sys.path
-import utils
+#import utils
+import FBSimilarityMeasures as fb
 
 import numpy as np
-from gensim.models import Word2Vec
+#from gensim.models import Word2Vec
 from sklearn.preprocessing import normalize
 from scipy import spatial
 
@@ -13,8 +20,11 @@ from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
 from nltk.stem.porter import *
 
-
-
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+import matplotlib.pyplot as plt
 
 import _pickle as cPickle
 import pandas as pd
@@ -66,7 +76,7 @@ class bow(object):
         a = self.encode([sentenceA])
         b = self.encode([sentenceB])
         s = (1 - spatial.distance.cosine(a[0],b[0]))
-        print "word2vec score:", s
+        print("word2vec score:", s)
         return s
     
     
@@ -166,8 +176,9 @@ class featureBased(object):
     def __init__(self):
         print("featureBased init: loading word2vec model")
         self.stoplist = stopwords.words('english')
-        self.qs = quickScore()
+        #self.qs = quickScore()
         return
+        
     def pairFeatures(self, sentenceA, sentenceB):
         features = list()
         
@@ -178,6 +189,10 @@ class featureBased(object):
         features.append( len(''.join(set(sentenceB.replace(' ', '')))) ) 
         features.append( len(sentenceA.split()) )
         features.append( len(sentenceB.split()) ) 
+
+        features.append(fb.longestCommonsubstring(sentenceA, sentenceB))
+        features.append(fb.longestCommonSubseq(sentenceA, sentenceB))
+        features.append(fb.funcWordFreq(sentenceA, sentenceB))
         
         ## substring and n-gram features 
         for length in (3,5,7):
@@ -196,28 +211,154 @@ class featureBased(object):
         features.append( fuzz.token_set_ratio(sentenceA, sentenceB) )
         features.append( fuzz.token_set_ratio(sentenceA, sentenceB) ) 
         
+        """
         ## word semantic features
         
         for f in self.qs.pairFeatures(sentenceA, sentenceB, stemming = 0):
             features.append(f)
         for f in self.qs.pairFeatures(sentenceA, sentenceB, stemming = 1):
-            features.append(f)
-        
-        
-        
+            features.append(f)    
+        """
         return features
-        
 
-'''''
+    def createFeatures(self, data):
+        #create feature data frame
+        data['lenA'] = data.sentence_A.apply(lambda x: len(x))
+        data['lenB'] = data.sentence_B.apply(lambda x: len(x))
+        data['diff_len'] = abs(data.lenA - data.lenB)
+        data['len_charA'] = data.sentence_A.apply(lambda x: len(''.join(set(str(x).replace(' ', '')))))
+        data['len_charB'] = data.sentence_B.apply(lambda x: len(''.join(set(str(x).replace(' ', '')))))
+        data['len_wordA'] = data.sentence_A.apply(lambda x: len(str(x).split()))
+        data['len_wordB'] = data.sentence_B.apply(lambda x: len(str(x).split()))
+        data['ttrA'] = data.sentence_A.apply(lambda x: fb.ttr(x))
+        data['ttrB'] = data.sentence_B.apply(lambda x: fb.ttr(x))
+        
+        data['lcstr'] = data.apply(lambda row: fb.longestCommonsubstring(row['sentence_A'], row['sentence_B']), axis=1)
+        data['lcseq'] = data.apply(lambda row: fb.longestCommonSubseq(row['sentence_A'], row['sentence_B']), axis=1)
+        data['fwfreq'] = data.apply(lambda row: fb.funcWordFreq(row['sentence_A'], row['sentence_B']), axis=1)
+        #data['gst'] = data.apply(lambda row: fb.gst(row['sentence_A'], row['sentence_B'], 2), axis=1)
+        data['common_words'] = data.apply(lambda x: len(set(str(x['sentence_A']).lower().split()).intersection(set(str(x['sentence_B']).lower().split()))), axis=1)
+        data['fuzz_qratio'] = data.apply(lambda x: fuzz.QRatio(str(x['sentence_A']), str(x['sentence_B'])), axis=1)
+        data['fuzz_WRatio'] = data.apply(lambda x: fuzz.WRatio(str(x['sentence_A']), str(x['sentence_B'])), axis=1)
+        data['fuzz_partial_ratio'] = data.apply(lambda x: fuzz.partial_ratio(str(x['sentence_A']), str(x['sentence_B'])), axis=1)
+        data['fuzz_partial_token_set_ratio'] = data.apply(lambda x: fuzz.partial_token_set_ratio(str(x['sentence_A']), str(x['sentence_B'])), axis=1)
+        data['fuzz_partial_token_sort_ratio'] = data.apply(lambda x: fuzz.partial_token_sort_ratio(str(x['sentence_A']), str(x['sentence_B'])), axis=1)
+        data['fuzz_token_set_ratio'] = data.apply(lambda x: fuzz.token_set_ratio(str(x['sentence_A']), str(x['sentence_B'])), axis=1)
+        data['fuzz_token_sort_ratio'] = data.apply(lambda x: fuzz.token_sort_ratio(str(x['sentence_A']), str(x['sentence_B'])), axis=1)
+
+        data = data.drop(['pair_ID', 'sentence_A', 'sentence_B', 'entailment_judgment'], axis=1)
+        data.replace([np.inf, -np.inf], np.nan).dropna(how="all")
+        data = data.fillna(0)
+
+        return data
+
 if __name__ == '__main__':
+    """
     f = featureBased()
     print f.pairFeatures("Greatings my dear lady!", "Hi miss, where is Lady Gaga?")
     print f.pairFeatures("This is a good new book!", "full of great stories")
     print f.pairFeatures("Can't say anything", "But you said something")
     print f.pairFeatures("mantel", "layer")
     print len(f.pairFeatures("mantel", "layer"))
-'''
+    """
+
+    f = featureBased()
+    data = pd.read_csv("/home/ds/STS/data/SICK/SICK_train.txt", sep = '\t' , engine = 'python')
+    data.head
+    data = f.createFeatures(data)    
+    
+    Y_train = data['relatedness_score']
+    X_train = data.drop(['relatedness_score'], axis=1)
+
+    from sklearn import linear_model
+    regr = linear_model.LinearRegression()
+
+    regr.fit(X_train, Y_train)
+
+    f = featureBased()
+    data = pd.read_csv("/home/ds/STS/data/SICK/SICK_trial.txt", sep = '\t' , engine = 'python')
+    data.head
+    data = f.createFeatures(data)    
+    
+    Y_test = data['relatedness_score']
+    X_test = data.drop(['relatedness_score'], axis=1)
+    pred = regr.predict(X_test)
+
+    print('Fit a model X_train, and calculate MSE with Y_train:', np.mean((Y_train - regr.predict(X_train)) ** 2))
+    print('Fit a model X_train, and calculate MSE with X_test, Y_test:', np.mean((Y_test - regr.predict(X_test)) ** 2))
+    #print('score sick train', regr.score(X_test, pred))   
 
     
+    # SICK_train
+    f = featureBased()
+    data = pd.read_csv("/home/ds/STS/data/SICK/SICK_train.txt", sep = '\t' , engine = 'python')
+    data.head
+    data = f.createFeatures(data)    
     
-    
+    Y = data['relatedness_score']
+    X = data.drop(['relatedness_score'], axis=1)
+
+    from sklearn import linear_model
+    regr = linear_model.LinearRegression()
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.30, random_state=5) 
+    regr.fit(X_train, Y_train)
+    pred_train = regr.predict(X_train)
+    pred_test = regr.predict(X_test)
+
+    print('SICK train results')
+
+    print('Fit a model X_train, and calculate MSE with Y_train:', np.mean((Y_train - regr.predict(X_train)) ** 2))
+    print('Fit a model X_train, and calculate MSE with X_test, Y_test:', np.mean((Y_test - regr.predict(X_test)) ** 2))
+    #print('score sick train', regr.score(X_test, pred_test))
+    #data['features'] = data.apply(lambda row: f.pairFeatures(row['sentence_A'], row['sentence_B']), axis=1)  
+
+    # SICK_trial
+    print('SICK trial results')
+    data = pd.read_csv("/home/ds/STS/data/SICK/SICK_trial.txt", sep = '\t' , engine = 'python')
+    data.head
+    data = f.createFeatures(data)    
+
+    print('nan', np.any(np.isnan(data)))
+    print('finite?', np.all(np.isfinite(data)))
+
+
+    Y = data['relatedness_score']
+    X = data.drop(['relatedness_score'], axis=1)
+
+    from sklearn import linear_model
+    regr = linear_model.LinearRegression()
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.30, random_state=5) 
+    regr.fit(X_train, Y_train)
+    pred_train = regr.predict(X_train)
+    pred_test = regr.predict(X_test)
+
+    print('Fit a model X_train, and calculate MSE with Y_train:', np.mean((Y_train - regr.predict(X_train)) ** 2))
+    print('Fit a model X_train, and calculate MSE with X_test, Y_test:', np.mean((Y_test - regr.predict(X_test)) ** 2))
+    #print('score sick trial', regr.score(X_test, pred_test))
+
+    # SICK_test_annotated
+    print('SICK_test_annotated results')
+    data = pd.read_csv("/home/ds/STS/data/SICK/SICK_test_annotated.txt", sep = '\t' , engine = 'python')
+    data.head
+    data = f.createFeatures(data)    
+
+    print('nan', np.any(np.isnan(data)))
+    print('finite?', np.all(np.isfinite(data)))
+
+
+    Y = data['relatedness_score']
+    X = data.drop(['relatedness_score'], axis=1)
+
+    from sklearn import linear_model
+    regr = linear_model.LinearRegression()
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.30, random_state=5) 
+    regr.fit(X_train, Y_train)
+    pred_train = regr.predict(X_train)
+    pred_test = regr.predict(X_test)
+
+    print('Fit a model X_train, and calculate MSE with Y_train:', np.mean((Y_train - regr.predict(X_train)) ** 2))
+    print('Fit a model X_train, and calculate MSE with X_test, Y_test:', np.mean((Y_test - regr.predict(X_test)) ** 2))
+    #print('score sick annotated', regr.score(X_test, pred_test))
