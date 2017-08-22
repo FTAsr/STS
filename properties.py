875ca20a2e4b8fd357e35f8e5ff8c04b2a2858d2
Shashi <shashank@umail.iu.edu>
2017-08-21 22:06:28 -0700

    Update properties.py

--- properties.py
+++ properties.py
@@ -1,8 +1,8 @@
 ## Contains the configurable parameters for the Short Answer Scoring system.
 
 ## Set the required grading scale as True
-GRADING_SCALE_MULTICLASS = True
-GRADING_SCALE_REAL_0_5 = False
+GRADING_SCALE_MULTICLASS = False
+GRADING_SCALE_REAL_0_5 = True
 GRADING_SCALE_REAL_0_1 = False
 GRADING_SCALE_Integer_0_5 = False
 GRADING_SCALE_LABELS = ['Incorrect', 'Partially Correct', 'Correct']
@@ -18,7 +18,7 @@ BATCH_MODE = False
 LOWER_LIMIT = 15.0
 
 ## Set the pre-trained classifier
-BEST_CLASSIFIER_COLLEGE = 'bow+fb+college.file'
+BEST_CLASSIFIER_COLLEGE = 'feed+fb+college.file'
 BEST_CLASSIFIER_1A = 'bow+fb+1A.file'
 BEST_CLASSIFIER_2A = 'bow+fb+2A.file'
 BEST_CLASSIFIER_SICK = 'bow+fb+sick.file'
